c534822c42543ad9df73d936c770c36ce1626284
yashth534 <yash.thakur@xpectrum-ai.com>
2025-07-14 19:19:53 +0530

    url fix

--- frontend/app/(admin)/auth/ProtectedRoute.tsx
+++ frontend/app/(admin)/auth/ProtectedRoute.tsx
@@ -12,8 +12,7 @@ export function ProtectedRoute({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   useEffect(() => {
     if (!loading && !user) {
-      //window.location.href = "https://181249979.propelauthtest.com/propelauth/oauth/authorize?redirect_uri=http://localhost:8000/oauth/callback&client_id=29d33276022f9b66722356fb92930464&response_type=code&state=fckchjcsdjcsdjcsfdhjcsfd";
-      redirectToLoginPage();
+      window.location.href = "https://181249979.propelauthtest.com/propelauth/oauth/authorize?redirect_uri=http://localhost:8001/api/v1/oauth2callback&client_id=29d33276022f9b66722356fb92930464&response_type=code&state=fckchjcsdjcsdjcsfdhjcsfd";
       
     }
   }, [loading, user]);
