a16df95cbbfca90e252213e5d015e7a24808c4d4
KarthikKonduru-xpectrum <karthikkonduru@xpectrum-ai.com>
2025-09-14 11:42:01 +0530

    Gmail tab

--- frontend-developer/app/components/index.ts
+++ frontend-developer/app/components/index.ts
@@ -2,6 +2,7 @@ export { default as AgentsTab } from './AgentsTab';
 export { default as PhoneNumbersTab } from './PhoneNumbersTab';
 export { default as SMSTab } from './SMSTab';
 export { default as WhatsAppTab } from './WhatsAppTab';
+export { default as GmailTab } from './GmailTab';
 export { default as OverviewTab } from './OverviewTab';
 export { default as Navbar } from './Navbar';
 export { default as OrgSetup } from './OrgSetup';
